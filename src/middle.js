e7cf46d4f96af3226fb85d677fa2d4cb3a0727d2
Trikcode <ayesiganobert@gmail.com>
2021-10-31 02:12:49 +0300

    ciroyelle

--- src/middle.js
+++ src/middle.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
-import vid from './images/vid.mp4'
 
 function Middle() {
   return (
@@ -8,7 +7,7 @@ function Middle() {
       <div className='middle'>
         <div className='middle-center'>
           <ReactPlayer
-            url={vid}
+            url='https://res.cloudinary.com/trikerl/video/upload/v1635635456/computer/vid_pivymn.mp4'
             playing={true}
             width='100%'
             height='100%'
